b0c4f0238133c84bb841fa07df66344c303f009a
Wellington Garcia <garciawell@gmail.com>
2019-09-08 02:12:28 -0300

    Api mock axios

--- __tests__/store/sagas/rocks.test.js
+++ __tests__/store/sagas/rocks.test.js
@@ -1,13 +1,33 @@
 import { runSaga } from 'redux-saga';
+import MockAdapter from 'axios-mock-adapter';
+import api from '~/services/api';
+
 import { getRocks } from '~/store/modules/rocks/sagas';
-import { getRocksSuccess } from '~/store/modules/rocks/actions';
+import {
+  getRocksSuccess,
+  getRocksFailure,
+} from '~/store/modules/rocks/actions';
+
+const apiMock = new MockAdapter(api);
 
 describe('Rocks saga', () => {
   it('Should be able to fetch techs', async () => {
     const dispatch = jest.fn();
 
+    apiMock.onGet('rocks').reply(200, ['Node.js']);
+
     await runSaga({ dispatch }, getRocks).toPromise();
 
     expect(dispatch).toHaveBeenCalledWith(getRocksSuccess(['Node.js']));
+  });
+
+  it('Should be fail when api return error', async () => {
+    const dispatch = jest.fn();
+
+    apiMock.onGet('rocks').reply(500);
+
+    await runSaga({ dispatch }, getRocks).toPromise();
+
+    expect(dispatch).toHaveBeenCalledWith(getRocksFailure());
   });
 });
