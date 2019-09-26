1a4f55c47f3d8794ad950060e574d825caa29e49
Wellington Garcia <garciawell@gmail.com>
2019-09-25 23:36:37 -0300

    Render Project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import logo from './assets/rocket.svg';
 import astronaut from './assets/astronaut.svg';
+import Techlist from '~/components/Techlist';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <img className="logo" src={logo} alt="Rocketseat Logo" />
+      {/* <img className="logo" src={logo} alt="Rocketseat Logo" />
       <h2>Welcome to OmniStack!</h2>
       <p>
         Edit <code>src/App.js</code> and save to reload.
@@ -14,7 +15,8 @@ function App() {
       <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
         Learn React
       </a>
-      <img className="astronaut" src={astronaut} alt="Astronaut" />
+      <img className="astronaut" src={astronaut} alt="Astronaut" /> */}
+      <Techlist />
     </div>
   );
 }
